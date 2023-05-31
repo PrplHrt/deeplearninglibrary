33360ca64f65ea85273b839f612ed81ff35fc8b4
Eyad Mohamed Ali <eyad.azim@gmail.com>
2023-05-31 18:25:55 +0400

    Changed MSE from summed square loss to mean square error

--- prplnet/loss.py
+++ prplnet/loss.py
@@ -2,6 +2,8 @@
 A loss function measures how good our predictions are,
 we can use this to adjust the parameters of our network
 """
+#TODO: #6 Implement cross entropy and other classic loss functions
+#TODO: #5 Add in some kind of regularization term to penalize larger weights (you'd have to change API)
 import numpy as np
 
 from prplnet.tensor import Tensor
@@ -15,15 +17,11 @@ class Loss:
     
 class MSE(Loss):
     """
-    MSE is mean squared error, although we're
-    just going to do total squared error
+    MSE is mean squared error
     """
-    #TODO: #1 Change this to Mean Squared Error
     def loss(self, predicted: Tensor, actual: Tensor) -> float:
-        return np.sum((predicted - actual)**2)
+        return np.mean((predicted - actual)**2)
     
     def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
         return 2 * (predicted - actual)
 
-#TODO: Implement cross entropy and other classic loss functions
-#TODO: Add in some kind of regularization term to penalize larger weights (you'd have to change API)
